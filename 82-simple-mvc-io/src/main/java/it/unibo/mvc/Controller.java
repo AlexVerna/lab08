bad57a88d6e6e2011437a65702b0802ebe7dc10c
Alessandro <alessandro.verna@studio.unibo.it>
2022-11-16 22:46:49 +0100

    a few mods on class Controller.java

--- 82-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 82-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,22 @@
 package it.unibo.mvc;
 
+import java.io.File;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
 
+    private static final String HOME = System.getProperty("user.home");
+    private static final String DEFAULT_FILE = "output.txt";
+
+    File input = new File(HOME + File.separator + DEFAULT_FILE);
+    
+    public File getFile() {
+        return input;
+    }
+
+    public String getPath(File input) {
+        return input.getPath();
+    }
 }
